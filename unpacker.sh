553fece08d22b05f306438a367d7f81c0546fc3d
moukaddam <mhd.moukaddam@gmail.com>
2017-07-26 16:34:35 +0100

    updating the unpacker shell script to set the necessary env. variables, fixing the /home/mhd/Packages/Unpacker variable

--- unpacker.sh
+++ unpacker.sh
@@ -1,20 +1,38 @@
-#!bin/bash   
-CUR_DIR="$PWD" 
-SCRIPTFILE=$0 
-if [ "${SCRIPTFILE}" = "-bash" ] ; then  
-  SCRIPTFILE=${BASH_ARGV[0]} 
-elif [ "${SCRIPTFILE}" = "bash" ] ; then  
-  SCRIPTFILE=${BASH_ARGV[0]} 
-fi 
+#! /bin/bash  
 
-SCRIPTPATH="${SCRIPTFILE}" 
-export UNPACKER=$(dirname $SCRIPTPATH)
+#echo changes to the envir. variables will be printed on screen
+ 
+# find script path
+if [ -n "$ZSH_VERSION" ]; then
+   SCRIPTPATH="$( cd "$( dirname "${(%):-%x}" )" && pwd )"
+   #echo $ZSH_VERSION ok scriptpath $SCRIPTPATH
+elif [ -n "$BASH_VERSION" ]; then
+   SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+   #echo $BASH_VERSION ok scriptpath $SCRIPTPATH
+else
+   echo "neither bash or zsh is used, abort"
+   exit 1
+fi
+
+
+# export UNPACKER environment variable
+export UNPACKER=$SCRIPTPATH
+#echo unpacker directory $UNPACKER
+
 # mac os x case
-if [ "${NPARCH}" = "Darwin" ] ; 
+if [ "${UNPARCH}" = "Darwin" ] ; 
 then
   export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$UNPACKER/lib
   export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$UNPACKER/UpCore
+  #echo updating the library path $DYLD_LIBRARY_PATH 
 else 
   export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$UNPACKER/lib
 	export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$UNPACKER/UpCore
+	#echo updating the library path $LD_LIBRARY_PATH
 fi
+
+# no bin is used
+export PATH=$PATH:$UNPACKER
+#echo updating the path $PATH
+
+
