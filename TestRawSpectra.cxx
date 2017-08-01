6c7aecfc0f8c08ed6143f40e664eb9eadbb93bdb
moukaddam <mhd.moukaddam@gmail.com>
2017-08-01 20:30:44 +0100

    taking off charge integration

--- TestRawSpectra.cxx
+++ TestRawSpectra.cxx
@@ -10,8 +10,8 @@ void TestRawSpectra(int entries=100000){
   int det = 1; 
   for(unsigned int i = 1 ; i < 17 ; i++){
    c0->cd(i); 
-   //rawdata->Draw(Form("fSharc_StripFront_Energy:fSharc_StripBack_Energy>>h0%i(10000,0,300000,1000,0,300000)",i),Form("fSharc_StripFront_DetectorNbr==%i && fSharc_StripFront_DetectorNbr==%i",i,i),"colz",entries,0) ;
-  rawdata->Draw(Form("fSharc_StripBack_Energy>>h0%i%i(1000,0,300000)",det,i),Form("fSharc_StripFront_StripNbr==%i & fSharc_StripFront_DetectorNbr==%i",i, det),"colz",entries,0) ;
+   //rawdata->Draw(Form("fSharc_StripFront_Energy:fSharc_StripBack_Energy>>h0%i(10000,0,10000,1000,0,10000)",i),Form("fSharc_StripFront_DetectorNbr==%i && fSharc_StripFront_DetectorNbr==%i",i,i),"colz",entries,0) ;
+  rawdata->Draw(Form("fSharc_StripBack_Energy>>h0%i%i(1000,0,10000)",det,i),Form("fSharc_StripFront_StripNbr==%i & fSharc_StripFront_DetectorNbr==%i",i, det),"colz",entries,0) ;
   }
  
  //sharc
@@ -19,7 +19,7 @@ void TestRawSpectra(int entries=100000){
   c1->Divide(2,2);
   for(unsigned int i = 1 ; i < 5 ; i++){
    c1->cd(i); 
-  rawdata->Draw(Form("fSharc_StripBack_Energy>>h1%i(1000,0,300000)",i),Form("fSharc_StripFront_DetectorNbr==%i",i),"colz",entries,0) ;
+  rawdata->Draw(Form("fSharc_StripBack_Energy>>h1%i(1000,0,10000)",i),Form("fSharc_StripFront_DetectorNbr==%i",i),"colz",entries,0) ;
   }
  
   //sharc
@@ -45,7 +45,7 @@ void TestRawSpectra(int entries=100000){
    c2->cd(3); 
     rawdata->Draw(Form("fTIG_Ge_SegmentNbr>>h23(11,0,11)"),"","",entries,0) ;
    c2->cd(4); 
-    rawdata->Draw(Form("fTIG_Ge_Energy/125>>h24(2000,0,10000)"),"","",entries,0) ;
+    rawdata->Draw(Form("fTIG_Ge_Energy>>h24(2000,0,10000)"),"","",entries,0) ;
    c2->cd(5); 
     rawdata->Draw(Form("fTIG_Ge_TimeCFD"),"","",entries,0) ;
    c2->cd(6); 
@@ -56,7 +56,7 @@ void TestRawSpectra(int entries=100000){
   c3->Divide(2,2);
   for(unsigned int i = 1 ; i < 5 ; i++){
    c3->cd(i); 
-    rawdata->Draw(Form("fTIG_Ge_Energy/125>>h3%i(2000,0,10000)",i),Form("fTIG_Ge_CloverNbr==6 && fTIG_Ge_CrystalNbr==%i && fTIG_Ge_SegmentNbr==0 ",i),"",entries,0) ; 
+    rawdata->Draw(Form("fTIG_Ge_Energy>>h3%i(2000,0,10000)",i),Form("fTIG_Ge_CloverNbr==6 && fTIG_Ge_CrystalNbr==%i && fTIG_Ge_SegmentNbr==0 ",i),"",entries,0) ; 
   }
 
  }
