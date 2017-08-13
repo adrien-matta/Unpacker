c3d8560bd9b491d39625a16ccd191e2e9ee07c3d
moukaddam <mhd.moukaddam@gmail.com>
2017-08-13 14:32:51 +0100

    esthetics

--- FolderScript.bash
+++ FolderScript.bash
@@ -1,15 +1,22 @@
 #! /bin/bash
-# ./FolderScript.bash /Users/moukaddam/Work/TRIUMF/S1719/MidasData/   /Users/moukaddam/Work/TRIUMF/S1719/RawData/
+
 for i in $1*.mid
 do
+  echo Working on $i
   DATANAME=${i/$1/$2}
   DATANAME=${DATANAME/run/data}
   DATANAME=${DATANAME/mid/root}
   SIZE=$(du $i | awk '{ print $1 }')
+  echo Working on $i, size: $SIZE
+
 if [ -f $DATANAME ]
   then echo $i already treated, ignored
 else
-  ./unpacker $i --user-output-tree-name rawdata --no-bank-tree --user-output-path $2 --intial-load 1000000 --maximum-load 1000000
+  ./unpacker
+  #./unpacker $i --user-output-tree-name rawdata --no-bank-tree --user-output-path $2 --intial-load 1000000 --maximum-load 1000000
   #./unpacker  $i --user-output-tree-name rawdata  --user-output-path  $2   --bank-output-tree-name bankdata   --bank-output-path  $2 --intial-load 1000000 --maximum-load 1000000
 fi
 done
+
+# ./FolderScript.sh /Users/moukaddam/Work/TRIUMF/S1719/MidasData/   /Users/moukaddam/Work/TRIUMF/S1719/RawData/
+# ./FolderScript.sh /Volumes/ne23dp/MidasData/   /Volumes/ne23dp/RawData/
